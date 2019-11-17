05dcb1723ca6abccb89116a2e6cb2428db51d595
Israel Torres <israel.a.torres01@gmail.com>
2019-11-16 23:56:04 -0500

    styling pages for responsive design

--- social-media-app/src/components/listMessage/ListMessage.js
+++ social-media-app/src/components/listMessage/ListMessage.js
@@ -6,11 +6,17 @@ import { TiDelete } from 'react-icons/ti'
 const ListMessage = props => {
     return (
         <div className="ListMessage" key={props.id} style={styles.list}>
-            <img src={props.val.mImage} alt='User image'/>
-            <h1>{props.val.userFirst} {props.val.userLast}</h1>
-            <p>{props.val.datetime}</p>
-            <p>{props.val.message}</p>
-            <BtnDelete btnText="Delete" icon={<TiDelete style={styles.tiDelete} />} onClick={props.deleteMe}></BtnDelete>
+            <div style={styles.imgWrapper}>
+                <img src={props.val.mImage} alt='User image' style={styles.img}/>
+            </div>
+            <div style={styles.textWrapper}>
+                <h1 style={styles.h1}>{props.val.userFirst} {props.val.userLast}</h1>
+                <p>{props.val.message}</p>
+            </div>
+            <div style={styles.dateWrapper}>
+                <p style={styles.p}>{props.val.datetime}</p>
+                <BtnDelete btnText="Delete" icon={<TiDelete style={styles.tiDelete} />} onClick={props.deleteMe}></BtnDelete>
+            </div>
         </div>
     )
 }
@@ -18,12 +24,13 @@ export default ListMessage
 
 const styles = {
     list: {
-        // display: 'flex',
-        // flexDirection: 'column',
-        // justifyContent: 'flex-start',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         width: '65%',
         margin: '20px auto',
-        padding: '20px',
+        padding: '0px 20px 10px 20px',
         backgroundColor: '#ffffff',
         color: '#040B71',
         borderRadius: '5px',
@@ -33,8 +40,7 @@ const styles = {
     h1: {
         color: '#040B71',
         fontSize: '1.25em',
-        fontWeight: '800',
-        marginBottom: '10px'
+        fontWeight: '800'
     },
     tiDelete: {
         position: 'absolute',
@@ -42,5 +48,35 @@ const styles = {
         right: '15px',
         width: '1.2em',
         height: '1.2em'
+    },
+    img: {
+        borderRadius: '50px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+    },
+    imgWrapper: {
+        width: '10%'
+    },
+    textWrapper: {
+        width: '75%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        padding: '15px 0px',
+    },
+    dateWrapper: {
+        width: '15%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        textAlign: 'right',
+        padding: '0px'
+    },
+    p: {
+        marginBottom: '50px',
+        marginRight: '5px'
     }
 }
