1e98dd3d1d031fd1a7706ced95753f44d69f8c9a
Israel Torres <israel.a.torres01@gmail.com>
2019-12-02 23:53:09 -0500

    Updates

---
+++ precache-manifest.78a37ae74825be2f978876288bd3d3a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccefe2ea2a58a22529cfdddfd1b15708",
+    "url": "/PRW/index.html"
+  },
+  {
+    "revision": "dbb2ed8a9f9faa677345",
+    "url": "/PRW/static/css/2.e091d897.chunk.css"
+  },
+  {
+    "revision": "2c183e96fc1285d60242",
+    "url": "/PRW/static/css/main.187e5216.chunk.css"
+  },
+  {
+    "revision": "dbb2ed8a9f9faa677345",
+    "url": "/PRW/static/js/2.e69e5467.chunk.js"
+  },
+  {
+    "revision": "2c183e96fc1285d60242",
+    "url": "/PRW/static/js/main.04666185.chunk.js"
+  },
+  {
+    "revision": "81bd525687744ce75d6d",
+    "url": "/PRW/static/js/runtime-main.4e06ecb9.js"
+  },
+  {
+    "revision": "aab912ae181d26f0d78c0b1c13453473",
+    "url": "/PRW/static/media/best-seller.aab912ae.png"
+  },
+  {
+    "revision": "13caffc6adbe9b2d5541bfcef0a5e630",
+    "url": "/PRW/static/media/logo.13caffc6.png"
+  }
+]);
